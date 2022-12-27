0a2b00e54980bafefc8a5612db80d9591f431110
cnem255 <caydennemec@icloud.com>
2022-12-26 19:05:35 -0500

    Initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
 
 function App() {
-  return (
-    <div className="h-screen bg-mountain">
-      <h1 className="text-8xl">Hello world!</h1>
+  return (    
+    <div className="h-screen w-screen bg-cover center bg-mountain">
+      <div className="frosted-glass card-md hover:card-md2 center">
+        <h1 className="font-source_code_pro center h-full w-full hover:text-[26.6px]">Source Code Pro Font</h1>
+      </div>
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,34 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,png,jpg}",
+    "./src/**/*.{js,ts,jsx,tsx,png,jpg,css}",
   ],
   theme: {
     extend: {
       backgroundImage: {
         "mountain": "url('./assets/img/mountain.jpg')"
+      },
+
+      fontFamily: {
+        quicksand: "'Quicksand', serif",
+        source_code_pro: "'Source Code Pro', monospace"
+      },
+
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
       }
     },
   },
