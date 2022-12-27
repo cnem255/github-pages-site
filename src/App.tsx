780b4184212cca27cfd1ed25add2f674782fc14b
cnem255 <caydennemec@icloud.com>
2022-12-26 21:41:01 -0500

    Added Headless UI and HeroIcons

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="bg-mountain h-full w-full bg-cover scroll-smooth">
-        <img src="/src/assets/img/logo.jpg" alt="" className="h-28 w-28 rounded-xl ml-[100vw / 2]"/>
-      <div className="frosted-glass-light top-bar-s dark:frosted-glass-dark shadow-lg">
-        <h1 className="dark:text-white text-slate-900 font-bebas_neue">
-          Bebas Neue Font
-        </h1>
-      </div>
-      <div className="frosted-glass-light dark:frosted-glass-dark w-screen h-96 mt-[800px] container">
-
-      </div>
+    <div>
+      
     </div>
   );
 }
